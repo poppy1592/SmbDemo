50dc2415a106a4971eb766c309b35d6a1439429e
Sally <wppawz@163.com>
2021-07-11 18:50:53 +0800

    Add files via upload

---
+++ idea_test/src/main/java/com/poppy/smb/SmbdemoApplication.java
@@ -0,0 +1,13 @@
+package com.poppy.smb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmbdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmbdemoApplication.class, args);
+    }
+
+}
---
+++ idea_test/src/test/java/com/poppy/smb/SmbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poppy.smb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmbdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
